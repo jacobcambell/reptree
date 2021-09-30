5b7bbb470560cc9cad63f3b7e0b9eda5c86d554e
Jacob Cambell <jacob@jacobcambell.com>
2021-09-30 13:23:21 -0600

    Handle login form and error/success messages

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,7 +1,52 @@
 import styles from './Login.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+
+    const history = useHistory();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [errormessage, setErrormessage] = useState(null);
+    const [successmessage, setSuccessmessage] = useState(null);
+
+    const handleForm = () => {
+        // Clear messages at the start of every form submit
+        setErrormessage(null);
+        setSuccessmessage(null);
+
+        if (
+            email.length === 0 ||
+            password.length === 0
+        ) {
+            setErrormessage('Please fill out the entire form');
+            return;
+        }
+
+        axios.post(process.env.REACT_APP_API_ENDPOINT + '/login', {
+            email,
+            password
+        }, { withCredentials: true })
+            .then((res) => {
+                if (res.data.error) {
+                    setErrormessage(res.data.message);
+                }
+                else {
+                    setSuccessmessage('Successfully logged in, redirecting...');
+
+                    // Save logged in status to localStorage
+                    localStorage.setItem('logged_in', true);
+
+                    setTimeout(() => {
+                        history.push('/dashboard');
+                    }, 1500);
+                }
+            })
+    }
+
     return (
         <div>
             <div className="container my-3">
@@ -10,14 +55,27 @@ const Login = () => {
                         <h3 className="card-title mb-3">Login</h3>
 
                         <label>Email Address</label>
-                        <input type="text" className="form-control form-control mb-3"></input>
+                        <input onChange={(e) => { setEmail(e.target.value) }} type="text" className="form-control form-control mb-3"></input>
 
                         <label>Password</label>
-                        <input type="text" className="form-control form-control mb-3"></input>
+                        <input onChange={(e) => { setPassword(e.target.value) }} type="password" className="form-control form-control mb-3"></input>
+
+                        {
+                            (errormessage !== null) &&
+                            <div className="alert alert-danger" role="alert">
+                                {errormessage}
+                            </div>
+                        }
+                        {
+                            (successmessage !== null) &&
+                            <div className="alert alert-success" role="alert">
+                                {successmessage}
+                            </div>
+                        }
 
                         <div className="row">
                             <div className="col-6 col-lg-3 mb-3 mb-lg-0">
-                                <button className="btn btn-success">Login</button>
+                                <button onClick={handleForm} className="btn btn-success">Login</button>
                             </div>
                             <div className="col-6 col-lg-9 d-flex align-items-center">
                                 <Link to="/register" className="text-muted">New to RepTree?</Link>
