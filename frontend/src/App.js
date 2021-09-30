4d645f89d36c691f39512b24a0be8b0868079c22
Jacob Cambell <jacob@jacobcambell.com>
2021-09-30 14:45:22 -0600

    Add fontawesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,17 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <Router>
-      <Navbar></Navbar>
-
       <Switch>
         <Route exact path="/">
+          <Navbar></Navbar>
           <Home></Home>
         </Route>
         <Route exact path="/register">
+          <Navbar></Navbar>
           <Register></Register>
         </Route>
         <Route exact path="/login">
+          <Navbar></Navbar>
           <Login></Login>
         </Route>
         <Route exact path="/dashboard">
