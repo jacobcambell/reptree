4dfdb5e5550711b7cff77afc666ea2f3a88dd036
Jacob Cambell <jacob@jacobcambell.com>
2021-09-30 12:45:53 -0600

    Minor changes

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-dark bg-dark">
-            <Link to="/" class="navbar-brand mx-3" href="#">RepTree</Link>
+        <nav className="navbar navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand mx-3" href="#">RepTree</Link>
             <div className="form-inline mx-3">
                 <Link to="/login" className="btn btn-secondary mx-2">Log In</Link>
                 <Link to="/register" className="btn btn-success">Register</Link>
