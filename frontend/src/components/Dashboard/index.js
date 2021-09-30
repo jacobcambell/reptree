bd1f5029582687c73f79a15c2ad7f075b8aab6c9
Jacob Cambell <jacob@jacobcambell.com>
2021-09-30 15:13:35 -0600

    remove active link for the time being

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -7,13 +7,14 @@ import Settings from './Settings';
 import AllCustomers from './AllCustomers';
 
 const Dashboard = () => {
+
     return (
         <div>
             <div className={`${styles.nav} d-flex flex-column navbar-dark bg-dark p-3`}>
                 <Link className="navbar-brand mb-3" to="/dashboard">RepTree</Link>
 
                 <ul className="nav nav-pills flex-column">
-                    <Link to="/dashboard" className="nav-link active" aria-current="page"><i className="fas fa-signal"></i>Dashboard</Link>
+                    <Link to="/dashboard" className="nav-link text-white" aria-current="page"><i className="fas fa-signal"></i>Dashboard</Link>
                     <Link to="/dashboard/all" className="nav-link text-white"><i className="fas fa-users"></i>All Customers</Link>
                     <Link to="/dashboard/create-customer" className="nav-link text-white"><i className="far fa-plus-square"></i>Create Customer</Link>
                     <Link to="/dashboard/my-brand" className="nav-link text-white"><i className="fas fa-star"></i>My Brand</Link>
