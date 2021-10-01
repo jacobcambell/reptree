cd67c7bc9d9f3821d1d9a17a47f7aee524ece923
Jacob Cambell <jacob@jacobcambell.com>
2021-10-01 09:13:22 -0600

    All Customers component logic

--- frontend/src/components/Dashboard/AllCustomers/index.js
+++ frontend/src/components/Dashboard/AllCustomers/index.js
@@ -1,7 +1,47 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const AllCustomers = () => {
+
+    // Array of objects with properties:
+    // name, phone, time, reminder_sent
+    const [customers, setCustomers] = useState([]);
+    useEffect(() => {
+        axios.post(process.env.REACT_APP_API_ENDPOINT + '/get-my-customers',
+            {},
+            { withCredentials: true })
+            .then(res => {
+                setCustomers(res.data);
+            })
+    }, []);
+
     return (
         <div>
+            <h3>All Customers</h3>
 
+            <table className="table">
+                <thead>
+                    <tr className="fw-bold">
+                        <td>Name</td>
+                        <td>Phone</td>
+                        <td>Remind In</td>
+                        <td>Reminder Sent?</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        customers &&
+                        customers.map((customer, i) => (
+                            <tr key={i}>
+                                <td>{customer.name}</td>
+                                <td>{customer.phone}</td>
+                                <td>{customer.time} Minutes</td>
+                                <td>{customer.reminder_sent ? `Sent` : `Not Sent`}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
