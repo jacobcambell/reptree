2beb1bd2437ac6df877161b03aa755b1e8c2f779
Jacob Cambell <jacob@jacobcambell.com>
2021-10-01 11:01:44 -0600

    Padding in navigation

--- frontend/src/components/Dashboard/index.js
+++ frontend/src/components/Dashboard/index.js
@@ -14,11 +14,11 @@ const Dashboard = () => {
                 <Link className="navbar-brand mb-3" to="/dashboard">RepTree</Link>
 
                 <ul className="nav nav-pills flex-column">
-                    <Link to="/dashboard" className="nav-link text-white" aria-current="page"><i className="fas fa-signal"></i>Dashboard</Link>
-                    <Link to="/dashboard/all" className="nav-link text-white"><i className="fas fa-users"></i>All Customers</Link>
-                    <Link to="/dashboard/create-customer" className="nav-link text-white"><i className="far fa-plus-square"></i>Create Customer</Link>
-                    <Link to="/dashboard/my-brand" className="nav-link text-white"><i className="fas fa-star"></i>My Brand</Link>
-                    <Link to="/dashboard/settings" className="nav-link text-white"><i className="fas fa-cog"></i>Settings</Link>
+                    <Link to="/dashboard" className="nav-link text-white" aria-current="page"><i className="fas fa-signal pe-2"></i>Dashboard</Link>
+                    <Link to="/dashboard/all" className="nav-link text-white"><i className="fas fa-users pe-2"></i>All Customers</Link>
+                    <Link to="/dashboard/create-customer" className="nav-link text-white"><i className="far fa-plus-square pe-2"></i>Create Customer</Link>
+                    <Link to="/dashboard/my-brand" className="nav-link text-white"><i className="fas fa-star pe-2"></i>My Brand</Link>
+                    <Link to="/dashboard/settings" className="nav-link text-white"><i className="fas fa-cog pe-2"></i>Settings</Link>
                 </ul>
             </div>
             <div className={`${styles.main} p-3`}>
