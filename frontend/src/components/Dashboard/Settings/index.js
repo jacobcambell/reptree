6a5b2dd46d27687d906a3da45fccdfc8d6b7ff7c
Jacob Cambell <jacob@jacobcambell.com>
2021-10-02 09:08:31 -0600

    Initial settings page layout

--- frontend/src/components/Dashboard/Settings/index.js
+++ frontend/src/components/Dashboard/Settings/index.js
@@ -1,6 +1,18 @@
 const Settings = () => {
     return (
-        <div></div>
+        <div>
+            <h3>Settings</h3>
+
+            <h5 className="pt-3">My Reminder Message</h5>
+            <h6 className="text-muted">Enter the message your customers will get when they are sent a text message.</h6>
+            <h6 className="text-muted">Tip: You can use the <b>((name))</b> flag to insert the customer's name, and use the ((company)) flag to insert your company's name.</h6>
+            <h6 className="text-muted">Note: Your review link will automatically be placed at the end of the message.</h6>
+
+
+            <textarea rows="7" className="form-control"></textarea>
+
+            <h5 className="pt-3">Message Preview:</h5>
+        </div>
     );
 }
 
