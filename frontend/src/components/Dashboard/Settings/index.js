07663f1130b63abc03bd117fa93c1f32fbc191c4
Jacob Cambell <jacob@jacobcambell.com>
2021-10-02 10:11:33 -0600

    Load user's SMS message into field

--- frontend/src/components/Dashboard/Settings/index.js
+++ frontend/src/components/Dashboard/Settings/index.js
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Settings = () => {
+
+    const [smsMessage, setSmsMessage] = useState('');
+
+    useEffect(() => {
+        // Load the user's SMS message
+        axios.post(process.env.REACT_APP_API_ENDPOINT + '/load-sms', {}, { withCredentials: true })
+            .then(res => {
+                setSmsMessage(res.data.sms_message);
+            })
+    }, []);
+
     return (
         <div>
             <h3>Settings</h3>
@@ -8,8 +22,7 @@ const Settings = () => {
             <h6 className="text-muted">Tip: You can use the <b>((name))</b> flag to insert the customer's name, and use the ((company)) flag to insert your company's name.</h6>
             <h6 className="text-muted">Note: Your review link will automatically be placed at the end of the message.</h6>
 
-
-            <textarea rows="7" className="form-control"></textarea>
+            <textarea rows="7" className="form-control" onChange={(e) => { setSmsMessage(e.target.value) }} value={smsMessage}></textarea>
 
             <h5 className="pt-3">Message Preview:</h5>
         </div>
