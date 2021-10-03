5799ea7bf70f4e8161aa03949a6199e1fbc5b0e5
Jacob Cambell <jacob@jacobcambell.com>
2021-10-03 08:40:55 -0600

    Update customer reminder_sent

--- backend/app.js
+++ backend/app.js
@@ -471,5 +471,10 @@ con.query(`SELECT
             to: results[i].phone, // Text this number
             from: process.env.twilio_fromPhone, // From a valid Twilio number
         })
+
+        // Update the customer as having been sent the reminder
+        con.query('UPDATE customers SET reminder_sent=1 WHERE customers.id=?', [results[i].id], (err, results) => {
+            if (err) throw err;
+        });
     }
 });
