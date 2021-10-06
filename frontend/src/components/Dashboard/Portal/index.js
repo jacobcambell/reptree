d9698fc77a4cc233f450c76a6bef74b9e2dfc71f
Jacob Cambell <jacob@jacobcambell.com>
2021-10-06 11:29:46 -0600

    Load analytics in Portal

--- frontend/src/components/Dashboard/Portal/index.js
+++ frontend/src/components/Dashboard/Portal/index.js
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Portal = () => {
+
+    const [analytics, setAnalytics] = useState({
+        totalCustomers: 0,
+        remindersSent: 0,
+        remindersOpened: 0
+    });
+
+    useEffect(() => {
+        axios.post(process.env.REACT_APP_API_ENDPOINT + '/get-analytics', {}, { withCredentials: true })
+            .then((res) => {
+                setAnalytics(res.data);
+            })
+    }, []);
+
     return (
         <div>
             <h3>Dashboard</h3>
@@ -6,15 +23,15 @@ const Portal = () => {
             <div className="row">
                 <div className="card col mx-4 align-items-center bg-primary text-light p-2">
                     <h6>Number of Customers</h6>
-                    <h2>12</h2>
+                    <h2>{analytics.totalCustomers}</h2>
                 </div>
                 <div className="card col mx-4 align-items-center bg-secondary text-light p-2">
                     <h6 className="align-self-center">Reminders Sent</h6>
-                    <h2>18</h2>
+                    <h2>{analytics.remindersSent}</h2>
                 </div>
                 <div className="card col mx-4 align-items-center bg-light text-dark p-2">
                     <h6>Reminders Opened</h6>
-                    <h2>7 (39%)</h2>
+                    <h2>{analytics.remindersOpened}</h2>
                 </div>
             </div>
         </div>
