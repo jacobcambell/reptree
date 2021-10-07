3acdcf8cbf553c3dd9e8ec7428345b31f9d40982
Jacob Cambell <jacob@jacobcambell.com>
2021-10-07 12:09:11 -0600

    Navbar class rework

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,13 +2,19 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-dark bg-dark">
-            <Link to="/" className="navbar-brand mx-3" href="#">RepTree</Link>
-            <div className="form-inline mx-3">
-                <Link to="/login" className="btn btn-secondary mx-2">Log In</Link>
-                <Link to="/register" className="btn btn-success">Register</Link>
+        <div className="container-fluid bg-dark g-0 py-2">
+            <div className="container-xxl">
+                <div className="row g-0">
+                    <div className="col px-3 d-flex align-items-center">
+                        <Link to="/" className="fs-4 text-white text-decoration-none" href="#"><i class="fas fa-seedling" style={{ color: '#0BD533' }}></i> RepTree</Link>
+                    </div>
+                    <div className="col-5 col-md-4 col-lg-3 col-xl-2 col-2 d-flex align-items-center justify-content-evenly">
+                        <Link to="/register" className="btn btn-primary">Try Free</Link>
+                        <Link to="/login" className="btn border border-light text-white text-decoration-none">Log In</Link>
+                    </div>
+                </div>
             </div>
-        </nav>
+        </div>
 
     );
 }
