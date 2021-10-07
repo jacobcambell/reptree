0731ba04782a446e07692e5e8c40437e9c3aed06
Jacob Cambell <jacob@jacobcambell.com>
2021-10-07 12:08:30 -0600

    Initial Jumbo design

---
+++ frontend/src/components/Home/Jumbo/index.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import styles from './Jumbo.module.css';
+
+const Jumbo = () => {
+    return (
+        <div className={styles.jumbo} style={{ backgroundImage: 'url(/images/jumbo-background.jpg)' }}>
+            <div className={styles.overlay}>
+                <div className="d-flex h-100 align-items-center text-center text-lg-start">
+                    <div className="container text-white">
+                        <h1 className="d-block">Grow your reputation. Grow your business</h1>
+                        <h4 className="d-block">Online reviews are the #1 purchasing decision made online.</h4>
+
+                        <Link to="/register" className="btn btn-primary fs-5 mt-3">Try Free</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbo;
