3706cc583eb8e50ae8520813ce1beb233a0dd07a
Jacob Cambell <jacob@jacobcambell.com>
2021-10-06 19:30:45 -0600

    More Popup detail

--- frontend/src/components/Dashboard/MyBrand/Popup/index.js
+++ frontend/src/components/Dashboard/MyBrand/Popup/index.js
@@ -8,9 +8,12 @@ const Popup = ({ networkName, setPopupOpen }) => {
 
     return (
         <div className={styles.popup}>
-            <div className="card p-3">
+            <div className="card p-3 col-3">
                 <button onClick={closePopup} type="button" class="btn-close align-self-end"></button>
-                <h4>Paste link for {networkName}</h4>
+                <h4>Enter {networkName} Link</h4>
+                <h5 className="text-muted fw-normal">This is the link your customers will be sent to when they click your {networkName} button.</h5>
+                <input type="text" className="form-control my-3" placeholder={`${networkName} Link`} />
+                <button className="btn btn-primary">Submit</button>
             </div>
         </div>
     );
