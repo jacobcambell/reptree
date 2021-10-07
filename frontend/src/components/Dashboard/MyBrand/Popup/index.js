013723221be966bee47593ac4acae0b62642b67e
Jacob Cambell <jacob@jacobcambell.com>
2021-10-06 20:01:02 -0600

    Small typos

--- frontend/src/components/Dashboard/MyBrand/Popup/index.js
+++ frontend/src/components/Dashboard/MyBrand/Popup/index.js
@@ -16,7 +16,7 @@ const Popup = ({ networkName, setPopupOpen, setNetwork }) => {
     return (
         <div className={styles.popup}>
             <div className="card p-3 col-3">
-                <button onClick={closePopup} type="button" class="btn-close align-self-end"></button>
+                <button onClick={closePopup} type="button" className="btn-close align-self-end"></button>
                 <h4>Enter {networkName} Link</h4>
                 <h5 className="text-muted fw-normal">This is the link your customers will be sent to when they click your {networkName} button.</h5>
                 <input onChange={(e) => { setLink(e.target.value) }} type="text" className="form-control my-3" placeholder={`Your ${networkName} Link`} />
