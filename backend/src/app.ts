601dd788a22af8e62cd748b2c020df2889371323
Jacob Cambell <jacob@jacobcambell.com>
2021-10-21 11:35:40 -0600

    Change environment variables naming

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,9 +15,9 @@ const jwt = require('jsonwebtoken');
 const AuthCheck = require('./AuthCheck');
 
 const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.MYSQL_DB_HOST,
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_DB_PASSWORD,
     database: 'reptree'
 });
 
